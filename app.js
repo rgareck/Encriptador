561f71033c20e228e17476b6903f71cdafc5e2a3
rgareck <120287553+rgareck@users.noreply.github.com>
2024-08-27 11:19:32 -0600

    app.js

--- app.js
+++ app.js
@@ -1,44 +1,66 @@
+let textoencriptad0 = false;
+
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
 function encriptartexto() {
-    let texto = document.getElementById("entradatexto").value;
-    let textoencriptado = texto
+    const texto = document.getElementById("entradatexto").value;
+    const textoencriptado = texto
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");   
     document.getElementById("salidatexto").textContent = textoencriptado;
-    if (textoencriptado !== "") {
+
+    textoencriptad0 = textoencriptado !== "";
+
+    if (textoencriptad0) {
         document.getElementById("botoncopiar").style.display = "block";
         document.getElementById("salidatexto").style.display = "block";
         document.getElementById("imageninicio").style.display = "none";
         document.getElementById('txtinicial1').style.display = "none";
         document.getElementById('txtinicial2').style.display = "none";
-        
     } else {
         document.getElementById("botoncopiar").style.display = "none";
         document.getElementById("reiniciar").style.display = "none";
-        
     }
 }
-function desencriptartexto() {
-    let texto = document.getElementById("salidatexto").textContent;
-    let decryptedText = texto
+function desencriptartexto(origen = 'salidatexto') {
+    let texto;
+    
+    if (origen === 'entradatexto') {
+        texto = document.getElementById("entradatexto").value;
+    } else {
+        texto = document.getElementById("salidatexto").textContent;
+    }
+
+    const textodesencriptado = texto
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
     
-    document.getElementById("salidatexto").textContent = decryptedText;
+    document.getElementById("salidatexto").textContent = textodesencriptado;
+
+    if (textodesencriptado !== "") {
+        document.getElementById("botoncopiar").style.display = "block";
+        document.getElementById("salidatexto").style.display = "block";
+        document.getElementById("imageninicio").style.display = "none";
+        document.getElementById('txtinicial1').style.display = "none";
+        document.getElementById('txtinicial2').style.display = "none";
+    } else {
+        document.getElementById("botoncopiar").style.display = "none";
+        document.getElementById("reiniciar").style.display = "none";
+    }
 }
+
 function copiartexto() {
-    let textocopiar = document.getElementById("salidatexto").textContent;
-    navigator.clipboard.writeText(textocopiar).then(() => {
+    let copiartexto = document.getElementById("salidatexto").textContent;
+    navigator.clipboard.writeText(copiartexto).then(() => {
         alert("Texto copiado al portapapeles");
     });
     condicionesIniciales();
